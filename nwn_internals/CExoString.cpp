2659c5f368ac2717869289a07b5f65b8e284a4ff
Honza Verbík <shadooow@seznam.cz>
2017-09-30 12:45:28 +0200

    c++ code using strcmp removed., functions will now call nwn engine originals

--- nwn_internals/CExoString.cpp
+++ nwn_internals/CExoString.cpp
@@ -8,6 +8,10 @@ DWORD CExoString__CExoString = 0x512AA0;//TODO
 DWORD CExoString__CExoString_c = 0x40B7E0;
 DWORD CExoString___CExoString = 0x40B9A0;
 DWORD CExoString__Format = 0x40C340;
+DWORD CExoString__operator_equal_CExoString = 0x40BB60;
+DWORD CExoString__operator_equal_c = 0x40BBF0;
+DWORD CExoString__operator_not_CExoString = 0x40BC70;
+DWORD CExoString__operator_not_c = 0x40BCF0;
 
 __declspec( naked ) CExoString::CExoString(){ __asm{ jmp dword ptr [CExoString__CExoString] }}
 __declspec( naked ) CExoString::CExoString(char const *str){ __asm{ jmp dword ptr [CExoString__CExoString_c] }}
@@ -20,53 +24,7 @@ __declspec( naked ) CExoString::~CExoString(){__asm{ jmp dword ptr [CExoString__
 
   __declspec( naked ) int CExoString::Format(int, char *, char) { __asm{ jmp dword ptr [CExoString__Format] }}
 
- bool CExoString::operator!=(const CExoString &Comp) {
-	bool result=false;
-	char *c = this->text;
-	char *c2 = Comp.text;
-	if (text && Comp.text) {
-		if (strcmp(c, c2))
-			result = true;
-	}
-	else {
-		result = c && *c || c2 && *c2;
-	}
-	return result;
-}
-
-bool CExoString::operator!=(char const *Comp) {
-	bool result=false;
-	char *c = this->text;
-	if (text && Comp) {
-		if (strcmp(text, Comp) != 0)
-			result = true;
-	}
-	else {
-		result = c && *c || Comp && *Comp;
-	}
-	return result;
-}
-
-bool CExoString::operator==(CExoString const &Comp) {
-	if (!text && !Comp.text) return true;
-	else if (!text && Comp.text || text && !Comp.text)
-		return false;
-	
-	//both have to be valid pointers at this point
-	if (strcmp(text, Comp.text))
-		return false;
-		
-	return true;
-}
-
-bool CExoString::operator==(char const *c) {
-	if (!text && !c) return true;
-	else if (!text && c || text && !c)
-		return false;
-	
-	//both have to be valid pointers at this point
-	if (strcmp(text, c))
-		return false;
-		
-	return true;
-}
+  __declspec( naked ) bool CExoString::operator==(CExoString const &Comp){ __asm{ jmp dword ptr [CExoString__operator_equal_CExoString] }}
+  __declspec( naked ) bool CExoString::operator==(char const *){ __asm{ jmp dword ptr [CExoString__operator_equal_c] }}
+  __declspec( naked ) bool CExoString::operator!=(CExoString const &Comp){ __asm{ jmp dword ptr [CExoString__operator_not_CExoString] }}
+  __declspec( naked ) bool CExoString::operator!=(char const *){ __asm{ jmp dword ptr [CExoString__operator_not_c] }}
