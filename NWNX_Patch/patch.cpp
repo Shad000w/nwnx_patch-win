4163d477c3ed05b90b455f92140180b43694efce
Honza Verbík <shadooow@seznam.cz>
2018-05-24 10:21:20 +0200

    fix for issue where lowering max hit points didn't updated current hit points

--- NWNX_Patch/patch.cpp
+++ NWNX_Patch/patch.cpp
@@ -621,6 +621,7 @@ short (__fastcall *CNWSCreature__GetMaxHitPoints)(CNWSCreature *pThis, void*, in
 
 short __fastcall CNWSCreature__GetMaxHitPoints_Hook(CNWSCreature *pThis, void *, int n)
 {
+	Log(2,"o CNWSCreature__GetMaxHitPoints start\n");
 	short retVal = CNWSCreature__GetMaxHitPoints(pThis,NULL,n);
 	retVal+= pThis->obj.obj_vartable.GetInt(CExoString("HP_BONUS"));
 	int hp_percent = pThis->obj.obj_vartable.GetInt(CExoString("HP_%"));
@@ -629,7 +630,12 @@ short __fastcall CNWSCreature__GetMaxHitPoints_Hook(CNWSCreature *pThis, void *,
 	hp_percent = (retVal/100)*hp_percent;
 	retVal+= hp_percent;
 	}
-	return retVal > 1 ? retVal : 1;
+	if(retVal < 1) retVal = 1;
+	if(pThis->obj.obj_hp_cur > retVal)
+	{
+		pThis->obj.obj_hp_cur = retVal;//fix for issue where lowering max hitpoints didn't updated current hitpoints (if they were higher than new max)
+	}
+	return retVal;
 }
 
 void __fastcall CNWRules__ReloadAll_Hook(CNWRules *pThis, void *)
